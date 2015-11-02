a48f3bf4e4795b7f55a2b3940b245f691a08791f
giorgosrevis <giorgosrevis@hotmail.com>
2015-11-02 13:14:44 +0200

    Homework 6 half done

---
+++ Homework 6/Homework 6/Homework 6 part 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+
+            int sick = array.Sum();
+
+            Console.WriteLine(sick);
+
+
+
+        }
+
+    }
+
+
+}
+
+
+
+
+        
---
+++ Homework 6/Homework 6/Homework 6 part 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_5_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int num = int.Parse(Console.ReadLine());
+
+            int first = 1;
+
+            Console.Write("{0}", first);
+
+            int second = 2;
+
+            Console.Write("{0}", first);
+
+            int sum = 0;
+
+            while (sum <= num)
+
+            {
+                sum = first + second;
+
+                Console.Write("{0}", sum);
+
+                first = second;
+                second = sum;
+
+            }
+        }
+    }
+}
